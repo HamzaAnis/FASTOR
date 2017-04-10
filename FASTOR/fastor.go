5758329f5ac1c72f3368b9bbb73783ba22eb4564
HamzaAnis <hamzaanis9514@gmail.com>
2017-04-10 21:53:30 +0500

    Reading and displying the html file

--- FASTOR/fastor.go
+++ FASTOR/fastor.go
@@ -6,13 +6,26 @@ import (
 	"os"
 )
 
-func cdcHandler(w http.ResponseWriter, r *http.Request) {
+//on every / request the handler will call this
+func torhandler(w http.ResponseWriter, r *http.Request) {
 	domain := r.URL.Path[1:7]
 	link := r.URL.Path[11:]
-	fmt.Println(r.URL.Path[1:])
+	// fmt.Println(r.URL.Path[1:])
+	// fmt.Println(r.URL.Path[11:])
+	if domain == "fastor" {
+		fmt.Fprintf(w, "Domain: %v     ", domain)
+		fmt.Fprintf(w, "Website:  %v   ", link)
+	} else {
+		fmt.Fprintf(w, "There is no services available for which you requested")
+		// html, er := ioutil.ReadFile("test.html")
+		// if er == nil {
+		// 	fmt.Println("The file is  read")
+		// } else {
+		// 	fmt.Println("The file is not read")
+		// }
+		// fmt.Fprint(w, string(html))
+	}
 
-	fmt.Fprintf(w, "Domain: %v     ", domain)
-	fmt.Fprintf(w, "Website:  %v   ", link)
 }
 
 func main() {
@@ -23,6 +36,6 @@ func main() {
 		port = "9999"
 	}
 	//default will go here
-	http.HandleFunc("/", cdcHandler)
+	http.HandleFunc("/", torhandler)
 	http.ListenAndServe(":"+port, nil)
 }
