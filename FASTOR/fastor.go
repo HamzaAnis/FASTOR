24cdf615e43c1d474c15d1833615730961bb6582
HamzaAnis <hamzaanis9514@gmail.com>
2017-04-10 21:10:21 +0500

    The domain and link separtaion

--- FASTOR/fastor.go
+++ FASTOR/fastor.go
@@ -6,16 +6,23 @@ import (
 	"os"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
 func cdcHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Testing1")
+	domain := r.URL.Path[1:7]
+	link := r.URL.Path[11:]
+	fmt.Println(r.URL.Path[1:])
+
+	fmt.Fprintf(w, "Domain: %v     ", domain)
+	fmt.Fprintf(w, "Website:  %v   ", link)
 }
 
 func main() {
-	http.HandleFunc("/fastor", cdcHandler)
-	http.ListenAndServe(":"+os.Args[1], nil)
+	port := ""
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	} else {
+		port = "9999"
+	}
+	//default will go here
+	http.HandleFunc("/", cdcHandler)
+	http.ListenAndServe(":"+port, nil)
 }
