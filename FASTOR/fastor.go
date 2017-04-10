fa8d0aba09eb812552e3b1548d6595ee1c48471f
HamzaAnis <hamzaanis9514@gmail.com>
2017-04-10 23:14:41 +0500

    The http page is get and displayed

--- FASTOR/fastor.go
+++ FASTOR/fastor.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
@@ -9,12 +11,21 @@ import (
 //on every / request the handler will call this
 func torhandler(w http.ResponseWriter, r *http.Request) {
 	domain := r.URL.Path[1:7]
-	link := r.URL.Path[11:]
-	// fmt.Println(r.URL.Path[1:])
-	// fmt.Println(r.URL.Path[11:])
+	link := r.URL.Path[8:]
+
 	if domain == "fastor" {
-		fmt.Fprintf(w, "Domain: %v     ", domain)
-		fmt.Fprintf(w, "Website:  %v   ", link)
+		fmt.Printf("Domain: %v\n", domain)
+		fmt.Printf("Website:  %v\n", link)
+		res, err := http.Get("http://" + link)
+		if err != nil {
+			log.Fatal(err)
+		}
+		robots, err := ioutil.ReadAll(res.Body)
+		fmt.Fprintf(w, string(robots))
+		res.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
 	} else {
 		fmt.Fprintf(w, "There is no services available for which you requested")
 		// html, er := ioutil.ReadFile("test.html")
