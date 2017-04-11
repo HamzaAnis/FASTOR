2c3bdd98881d40b828c9567208a636210e60d3e5
HamzaAnis <hamzaanis9514@gmail.com>
2017-04-11 20:19:42 +0500

    Getting type of the request

--- FASTOR/fastor.go
+++ FASTOR/fastor.go
@@ -7,12 +7,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"net/url"
+	"strings"
 )
+
+//A function to check the request and do the corrosponding action
+//E.g For image save the image to the file
+//If stylesheet save the stylesheet
+func handleStyle(link string){
+	u, err := url.Parse(link)
+	if err != nil {
+		panic(err)
+	}
+	path:=u.Path[1:] //to avoid the / at the beginning
+	filetype:=strings.Split(path,".")
+	fmt.Printf("Type is %v\n",filetype[len(filetype)-1])
+}
 
 //on every / request the handler will call this
 func torhandler(w http.ResponseWriter, r *http.Request) {
 	domain := r.URL.Path[1:7]
 	link :="http://" + r.URL.Path[8:]
+
+	handleStyle(link)
+
 	// if domain == "fastor" {
 		fmt.Printf("Domain: %v\n", domain)
 		fmt.Printf("Website:  %v\n", link)
